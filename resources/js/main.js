588e2b65c765905748bdad019b55b47684f03043
Dean <gao.dean@hotmail.com>
2022-09-12 07:43:14 +1000

    chore: clean slate

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,1 @@
-let getUsername = async () => {
-  const key = NL_OS == 'Windows' ? 'USERNAME' : 'USER';
-  let value = '';
-  try {
-      value = await Neutralino.os.getEnv(key);
-  }
-  catch(err) {
-      console.error(err);
-  }
-  document.getElementById('name').innerText = `Hello ${value}`;
-}
-
 Neutralino.init();
-getUsername();
