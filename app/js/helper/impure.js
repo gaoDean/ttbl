35af183709c830e13e96bdb16445f532655319b1
Dean <gao.dean@hotmail.com>
2022-09-21 09:53:17 +1000

    refactor: getToken, fetchToken() with neutralino.storage

--- app/js/helper/impure.js
+++ app/js/helper/impure.js
@@ -3,37 +3,55 @@
 	== impure functions, contact with the outside world == */
 
 let host="https://caulfieldsync.vercel.app/api"
-let cache="~/.cache/ttbl" // same as ttbl-cli
-let data="~/.local/share/ttbl"
-await Neutralino.filesystem.createDirectory(cache);
-await Neutralino.filesystem.createDirectory(data);
+
+async function curl(url) {
+	return await (await fetch(url)).json();
+}
 
 async function getToken() {
-	let token = await (await Neutralino.filesystem.readFile(data + "/token")).json();
-	token = token["token"]; // it's stored in the token property
+	let token_json = await Neutralino.storage.getData("token");
 	if (token == null) {
-		Neutralino.window.show();
-		window.location = "login.html"; // that should prevent the rest from executing I think...
+		throw new Error("No token found");
 	}
 	return token;
 }
 
+// export async function getTimetable()
 
-export async function ffetchTimetable(pastDays, futureDays) {
+export async function fetchToken(student_id, password) {
+	console.log("test");
+	let url = `${host}/token?username=${student_id}&password=${password}`;
+	let token = await curl(url);
+	if (Object.keys(json).indexOf("error") >= 0) { // if fails, returns -1
+		throw new Error(ttbl_json["error"]);
+	}
+	// the actual token is stored in the key "token"
+	await Neutralino.storage.setData("token", token["token"])
+}
+
+export async function fetchTimetable(pastDays, futureDays) {
+	console.log("fetching");
 
 	// extract date from class table
-	let getDate = (subject) => (subject["id"].substring(7))
+	let getDate = (subject) => (subject["id"].substring(7));
 
-	let token = await getToken();
-	console.log(token);
+	let token;
+	try {
+		token = await getToken();
+	}	catch(err) {
+		console.log(err);
+		window.location = "login.html";
+		Neutralino.window.show();
+		console.log("tst")
+		return;
+	}
 	let url = `${host}/timetable/${token}?dayMinus=${pastDays}&dayPlus=${futureDays}&shorten=true`;
-	let timetable_json = await (await fetch(url)).json();
+	let timetable_json = await curl(url)
 	Object.keys(timetable_json).indexOf(keytoFind);
 	if (Object.keys(json).indexOf("error") >= 0) { // if fails, returns -1
 		throw new Error(ttbl_json["error"]);
 	}
 	timetable_json = timetable_json["data"]["classes"]; // to access the raw data
-	// let cached_json = await (await Neutralino.filesystem.readFile(cache + "/timetable.json")).json();
 	// let cached = new Map(Object.entries(cached_json));
 	let copy = {};
 	for (let i in timetable_json) {
