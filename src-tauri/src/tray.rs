a0af4842ca01355ebb836a005afb1bb7dad41d0a
Dean <gao.dean@hotmail.com>
2022-09-27 14:06:14 +1000

    refactor: cargo fmt

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,13 +1,16 @@
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
-use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent};
+#[tauri::command]
+fn add_timetable_to_tray(timetable_json: String, msg: String, extra_msg: String) {
+    // let tray_handle =
+}
 
 pub fn tray_add_item(menu: SystemTrayMenu, id: &str, desc: &str) -> SystemTrayMenu {
     return menu.add_item(CustomMenuItem::new(id.to_string(), desc));
 }
 
 pub fn init_tray(opts: &[&str], desc: &[&str]) -> SystemTray {
-    let mut menu = SystemTrayMenu::new()
-        .add_native_item(SystemTrayMenuItem::Separator);
+    let mut menu = SystemTrayMenu::new().add_native_item(SystemTrayMenuItem::Separator);
 
     for i in 0..opts.len() {
         menu = tray_add_item(menu, opts[i], desc[i]);
@@ -23,10 +26,10 @@ pub fn handle_tray_event(app: &tauri::AppHandle, evt: tauri::SystemTrayEvent) {
             match id.as_str() {
                 "quit" => {
                     app.exit(0);
-                },
+                }
                 _ => {}
             }
-        },
+        }
         _ => {}
     }
 }
