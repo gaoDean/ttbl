01156fa28f284e2453b1f2531fd8a7947ef0ca5b
Dean <gao.dean@hotmail.com>
2022-10-12 11:23:20 +1100

    chore: add comments

--- src-tauri/src/time.rs
+++ src-tauri/src/time.rs
@@ -3,6 +3,7 @@ use std::{ops::Add, thread};
 
 use crate::impure;
 
+// from YYYYMMDD string to Date<Local>
 fn parse_ymd(ymd: String) -> Date<Local> {
     let ymd_sep: (i32, u32, u32) = (
         (&ymd[0..4]).parse().unwrap(),
@@ -12,8 +13,11 @@ fn parse_ymd(ymd: String) -> Date<Local> {
     return Local.ymd(ymd_sep.0, ymd_sep.1, ymd_sep.2);
 }
 
+// get the display msg from YYYYMMDD and no_classes
 pub fn get_msg(ymd: String, no_classes: bool) -> (String, String) {
     let date: Date<Local> = parse_ymd(ymd);
+
+    // date human-readable and the weekday (1-7)
     let fmt: String = date.format("%A, %e %B").to_string();
     let weekday: i32 = date.format("%u").to_string().parse().unwrap();
 
@@ -32,6 +36,7 @@ pub fn get_msg(ymd: String, no_classes: bool) -> (String, String) {
     return msg;
 }
 
+// add duration in days (int) to YYYYMMDD
 #[tauri::command]
 pub fn ymd_add(ymd: String, dur_in_days: i64) -> String {
     let date: Date<Local> = parse_ymd(ymd);
@@ -39,11 +44,13 @@ pub fn ymd_add(ymd: String, dur_in_days: i64) -> String {
     return date.add(duration).format("%Y%m%d").to_string();
 }
 
+// get the current time
 #[tauri::command]
 pub fn get_ymd() -> String {
     return Local::now().format("%Y%m%d").to_string();
 }
 
+// spawn the fetch timetable thread
 #[tauri::command]
 pub fn spawn_thread() {
     #[allow(unused)] // async not used, but needs await
