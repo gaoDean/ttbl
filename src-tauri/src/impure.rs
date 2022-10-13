0578315283a8dfbfde9f266a8dcb69df52121630
Dean <gao.dean@hotmail.com>
2022-10-13 11:06:17 +1100

    refactor: cargo fmt

--- src-tauri/src/impure.rs
+++ src-tauri/src/impure.rs
@@ -1,6 +1,6 @@
 use chrono::Local; // time
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, io::Write, fs};
+use std::{collections::HashMap, fs, io::Write};
 use tauri::api::http::{
     Client, ClientBuilder, HttpRequestBuilder, Response, ResponseData, ResponseType,
 };
