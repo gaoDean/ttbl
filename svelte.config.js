922b61f174e04f3a05f6d4b347178d6bdab7bbe5
Dean <gao.dean@hotmail.com>
2022-12-17 10:14:38 +1100

    chore: update sveltekit and add fallback page

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
+			fallback: '404.html'
 		}),
 	}
 };
