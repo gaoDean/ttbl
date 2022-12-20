61a9562286eb7922af696ac374a3aab42a123634
Dean <gao.dean@hotmail.com>
2022-12-21 09:58:44 +1100

    chore: update bump.sh

--- bump.sh
+++ bump.sh
@@ -31,13 +31,13 @@ case "$response" in
 		;;
 esac
 
-sed -E -i "s/(.*)\"version\".*/\1\"version\": \"$ver\",/" package.json
-sed -i "s/^version.*/version = \"$ver\"/" src-tauri/Cargo.toml
-sed -i "s/^\s\s\"version\".*/\t\t\"version\": \"$ver\"/" src-tauri/tauri.conf.json
-perl -i -p0e "s/name = \"app\"\nversion = \".*\"/name = \"app\"\nversion = \"$ver\"/" Cargo.lock
+sed -E -i "s/(.*)\"version\".*/\1\"version\": \"${ver}\",/" package.json
+sed -i "s/^version.*/version = \"${ver}\"/" src-tauri/Cargo.toml
+sed -i "s/^\s\s\"version\".*/\t\t\"version\": \"${ver}\"/" src-tauri/tauri.conf.json
+perl -i -p0e "s/name = \"app\"\nversion = \".*\"/name = \"app\"\nversion = \"${ver}\"/" Cargo.lock
 git add package.json
-git add Cargo.lock
 git add src-tauri/Cargo.toml
 git add src-tauri/tauri.conf.json
+git add Cargo.lock
 git commit -m "chore: version bump to v${ver}"
 git tag -a "v${ver}" -m "v${ver}" HEAD
