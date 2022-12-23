381cefe81230f344518aa5e04108a53ff06b154c
Dean <gao.dean@hotmail.com>
2022-12-24 10:33:16 +1100

    style: fmt

--- src/lib/background.js
+++ src/lib/background.js
@@ -4,11 +4,11 @@ import { fetchTimetable } from './fetch.js';
 
 // target is just hms not ymd
 export const syncTimetableProcess = async (targetTime) =>
-		setTimeout(async () => {
-			fetchTimetable(
-				await getData('token'),
-				(await getData('info')).id,
-				await getData('timetable'),
-			);
-			syncTimetableProcess(targetTime);
-		}, dayjs().diff(dayjs().startOf('day').add(targetTime), 'millisecond'));
+	setTimeout(async () => {
+		fetchTimetable(
+			await getData('token'),
+			(await getData('info')).id,
+			await getData('timetable'),
+		);
+		syncTimetableProcess(targetTime);
+	}, dayjs().diff(dayjs().startOf('day').add(targetTime), 'millisecond'));
